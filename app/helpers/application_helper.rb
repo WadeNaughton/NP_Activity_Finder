2dc5e4d8835ae340a0558a3b5dddb5a147b5909e
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-09-28 11:14:54 -0400

    Add 'states' method to check locale and add 'estados' method that includes translations of U.S. states

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+
+  def states 
+    if I18n.locale == :en
+      us_states
+    else
+      estados
+    end
+  end
+
   def us_states
     [
       ['Alabama', 'AL'],
@@ -51,6 +60,63 @@ module ApplicationHelper
       ['Virginia', 'VA'],
       ['Washington', 'WA'],
       ['West Virginia', 'WV'],
+      ['Wisconsin', 'WI'],
+      ['Wyoming', 'WY']
+    ]
+  end
+
+  def estados
+    [
+      ['Alabama', 'AL'],
+      ['Alaska', 'AK'],
+      ['Arizona', 'AZ'],
+      ['Arkansas', 'AR'],
+      ['California', 'CA'],
+      ['Colorado', 'CO'],
+      ['Connecticut', 'CT'],
+      ['Delaware', 'DE'],
+      ['Distrito de Columbia', 'DC'],
+      ['Florida', 'FL'],
+      ['Georgia', 'GA'],
+      ['Hawai', 'HI'],
+      ['Idaho', 'ID'],
+      ['Illinois', 'IL'],
+      ['Indiana', 'IN'],
+      ['Iowa', 'IA'],
+      ['Kansas', 'KS'],
+      ['Kentucky', 'KY'],
+      ['Louisiana', 'LA'],
+      ['Maine', 'ME'],
+      ['Maryland', 'MD'],
+      ['Massachusetts', 'MA'],
+      ['Michigan', 'MI'],
+      ['Minnesota', 'MN'],
+      ['Misisipi', 'MS'],
+      ['Misuri', 'MO'],
+      ['Montana', 'MT'],
+      ['Nebraska', 'NE'],
+      ['Nevada', 'NV'],
+      ['Nueva Hampshire', 'NH'],
+      ['Nueva Jersey', 'NJ'],
+      ['Nuevo Mexico', 'NM'],
+      ['Nueva York', 'NY'],
+      ['Carolina Del Norte', 'NC'],
+      ['Dakota Del Norte', 'ND'],
+      ['Ohio', 'OH'],
+      ['Oklahoma', 'OK'],
+      ['Oregón', 'OR'],
+      ['Pensilvania', 'PA'],
+      ['Puerto Rico', 'PR'],
+      ['Rhode Island', 'RI'],
+      ['Carolina Del Sur', 'SC'],
+      ['Dakota Del Sur', 'SD'],
+      ['Tennessee', 'TN'],
+      ['Texas', 'TX'],
+      ['Utah', 'UT'],
+      ['Vermont', 'VT'],
+      ['Virginia', 'VA'],
+      ['Washington', 'WA'],
+      ['Virginia Occidental', 'WV'],
       ['Wisconsin', 'WI'],
       ['Wyoming', 'WY']
     ]
