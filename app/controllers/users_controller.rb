c16f23f7cde50ae1b35c777b27bfa2fd52b9c235
Arnaldo Aparicio <apaarn2211@gmail.com>
2023-10-02 14:29:19 -0400

    Add require User to strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :username, :state, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :username, :state, :password, :password_confirmation)
   end
 end
